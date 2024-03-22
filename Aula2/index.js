41ef56d3429d20706c34c27eb122a50e623ab417
Igor Azevedo <igor.azevedo19@gmail.com>
2024-03-21 21:26:13 -0300

    Aula de viriaveis, console , alert e prompt

---
+++ Aula2/index.js
@@ -0,0 +1,53 @@
+//Declaração
+//var nome 
+//var sobrenome
+//var idade
+
+//atribuição
+//@String
+//nome = 'Igor'
+//sobrenome = 'Silva'
+//idade = 33
+
+//var marca = "Adidas"
+//console.log('marca')
+//Operadores numéricos + - * / ** %
+
+var primeiroNumero = 1
+var segundoNumero = 2
+var terceiroNumero = 3
+
+//soma de dois valores
+var soma = primeiroNumero + segundoNumero
+console.log("soma", soma)
+
+//Subtração de dois valores 
+var subtracao = segundoNumero - terceiroNumero
+console.log("subtracao", subtracao)
+
+//Multiplicação de dois valores 
+var divisao = primeiroNumero / segundoNumero
+console.log('divisao', divisao)
+
+//Resto da divisão
+var resto = primeiroNumero % terceiroNumero
+
+//Potencia
+var Potencia = primeiroNumero ** segundoNumero
+
+
+
+nome = 'Igor'
+sobrenome = 'Silva'
+anoAtual = '2024'
+idadeFinal = '34'
+
+var meuNome = nome + "" + sobrenome
+console.log("Nome:", meuNome)
+
+var idade = anoAtual - idadeFinal
+console.log("Idade:",  idade)
+
+
+var nomeInserido = prompt("Insira seu nome:")
+console.log("Nome inserido:" nomeInserido)
---
+++ Aula2/index1.js
@@ -0,0 +1,14 @@
+
+// var entrada = prompt("Insira uma letra")
+// var saida = entrada + " "  + "info saida"
+
+let numeroUm = parseInt(prompt("Digete o primero numero:"));
+let numerodois = parseInt(prompt("Digite o segundo numero:"));
+
+let soma = numeroUm + numerodois;
+
+console.log(soma);
+
+let multiplicacao = numeroUm * numerodois
+
+alert("O resultado é:" + multiplicacao)
